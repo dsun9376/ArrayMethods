712aec8991a3223ad771def8ee611dcf2bfacd9f
BT_1N3_26 <BT_1N3_26@BT_1N3_26.bktech.local>
2018-01-18 09:06:44 -0500

    d

---
+++ ArrayMethods/src/ArrayMethods1.java
@@ -0,0 +1,25 @@
+
+public class ArrayMethods1 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void insertionSort(int[] list1) {
+		
+	}
+	
+	public static void selectionSort(double [] list1) {
+	
+	}
+
+	public static void bubbleSort(String[] list1) {
+		
+	}
+	
+	public static void swap(int[] arr, int interval, int index 2) {
+		int temp = arr[index1]
+		arr[index1]= arr[index2]
+			arr[index2]
+}
+
